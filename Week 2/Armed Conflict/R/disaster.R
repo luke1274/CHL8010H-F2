bc0521ae48b8ae702458054d6f06e95b6c8c5526
Luke <lukeltb17@gmail.com>
2024-09-16 12:15:58 -0400

    Almost Finished

--- Week 2/Armed Conflict/R/disaster.R
+++ Week 2/Armed Conflict/R/disaster.R
@@ -7,6 +7,15 @@ subset <- disaster_data %>% select(Year, ISO, Disaster.Type)
 
 
 subset <- subset %>% mutate(
-  drought = ifelse(DisasterTyoe == "Drought", 1, 0),
+  drought = ifelse(DisasterType == "Drought", 1, 0),
   earthquake = ifelse(Disaster.Type == "Earthquake,1,0")
                                      )
+
+
+
+subset_multiple_disaster <- subset %>%
+  group_by(Year,ISO) %>%
+  summarise(
+    Drought = max(Disaster.Type == "Drought"),
+    Earthquake = max(Disaster.Type == "Earthquake")
+  )
