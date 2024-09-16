4b4887d6d8c21e045f9b3f3a231882944eed2353
Luke <lukeltb17@gmail.com>
2024-09-16 14:40:07 -0400

    test

--- Week 2/Maternal Mortality/R/maternal.R
+++ Week 2/Maternal Mortality/R/maternal.R
@@ -1,4 +1,4 @@
 library(dplyr)
 
 maternal_data <- read.csv("original/maternalmortality.csv")
-maternal_data_filtered <- maternal_data %>% select()
+maternal_data_filtered <- maternal_data %>% select(Country.Name, X2000:X2019) 
