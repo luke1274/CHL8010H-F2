9dbe6026f7309d7d1b3d682f1b14d3effa9ab0ca
Luke <lukeltb17@gmail.com>
2024-09-16 11:44:49 -0400

    Dummy Variables Added

--- Week 2/Armed Conflict/R/disaster.R
+++ Week 2/Armed Conflict/R/disaster.R
@@ -1,3 +1,12 @@
+library(dplyr)
+
 disaster_data <- read.csv("original/disaster.csv")
+disaster_set_filtered <- disaster_data %>% filter(Year>= 2000 & Year <= 2019, Disaster.Type %in% c("Drought","Earthquake"))
+
+subset <- disaster_data %>% select(Year, ISO, Disaster.Type)
 
 
+subset <- subset %>% mutate(
+  drought = ifelse(DisasterTyoe == "Drought", 1, 0),
+  earthquake = ifelse(Disaster.Type == "Earthquake,1,0")
+                                     )
