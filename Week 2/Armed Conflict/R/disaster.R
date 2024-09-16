351b9db88a540c3b9915bf7c7cba60941175ddc4
Luke <lukeltb17@gmail.com>
2024-09-16 12:25:12 -0400

    Finished

--- Week 2/Armed Conflict/R/disaster.R
+++ Week 2/Armed Conflict/R/disaster.R
@@ -1,21 +1,17 @@
 library(dplyr)
 
 disaster_data <- read.csv("original/disaster.csv")
-disaster_set_filtered <- disaster_data %>% filter(Year>= 2000 & Year <= 2019, Disaster.Type %in% c("Drought","Earthquake"))
+disaster_data_filtered <- disaster_data %>% filter(Year>= 2000 & Year <= 2019, Disaster.Type %in% c("Drought","Earthquake"))
 
-subset <- disaster_data %>% select(Year, ISO, Disaster.Type)
+subset <- disaster_data_filtered %>% select(Year, ISO, Disaster.Type)
 
 
 subset <- subset %>% mutate(
-  drought = ifelse(DisasterType == "Drought", 1, 0),
-  earthquake = ifelse(Disaster.Type == "Earthquake,1,0")
+  drought = ifelse(Disaster.Type == "Drought", 1, 0),
+  earthquake = ifelse(Disaster.Type == "Earthquake",1,0)
                                      )
 
 
 
 subset_multiple_disaster <- subset %>%
-  group_by(Year,ISO) %>%
-  summarise(
-    Drought = max(Disaster.Type == "Drought"),
-    Earthquake = max(Disaster.Type == "Earthquake")
-  )
+  group_by(Year,ISO) %>% summarize(drought = max(drought), earthquake = max(earthquake))
