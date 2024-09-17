e427df15a1bc6f8817e2f312d890e6e4d432065c
Luke <lukeltb17@gmail.com>
2024-09-16 20:11:11 -0400

    SAVED IN CLASS WORK!!

--- Week 2/Maternal Mortality/R/maternal.R
+++ Week 2/Maternal Mortality/R/maternal.R
@@ -1,4 +1,18 @@
 library(dplyr)
+library(tidyr)
 
-maternal_data <- read.csv("original/maternalmortality.csv")
+maternal_data <- read.csv("maternalmortality.csv")
 maternal_data_filtered <- maternal_data %>% select(Country.Name, X2000:X2019) 
+
+reshaped_data <- maternal_data_filtered %>%
+  pivot_longer(
+    cols = X2000:X2019,
+    names_to = "Year",
+    names_prefix = "X",
+    values_to = "MatMor",
+    values_drop_na = FALSE
+  )
+
+reshaped_data$Year <- as.numeric(reshaped_data$Year)
+
+head(reshaped_data)
