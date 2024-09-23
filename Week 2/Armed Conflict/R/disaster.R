90ac08e02ff06226ca9f9f626af8cdb6bc816b68
Luke <lukeltb17@gmail.com>
2024-09-23 11:49:46 -0400

    finished lagging year and binary armed conflict

--- Week 2/Armed Conflict/R/disaster.R
+++ Week 2/Armed Conflict/R/disaster.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-disaster_data <- read.csv("original/disaster.csv")
+disaster_data <- read.csv("disaster.csv")
 disaster_data_filtered <- disaster_data %>% filter(Year>= 2000 & Year <= 2019, Disaster.Type %in% c("Drought","Earthquake"))
 
 subset <- disaster_data_filtered %>% select(Year, ISO, Disaster.Type)
@@ -16,4 +16,17 @@ subset <- subset %>% mutate(
 subset_grouped <- subset %>%
   group_by(Year,ISO) %>% summarize(drought = max(drought), earthquake = max(earthquake))
 
-#Test
+
+#Week 3
+conflict_data <- read.csv("conflictdata.csv")
+
+conflict_data_bin <- conflict_data
+conflict_data_bin <- subset(conflict_data_bin, select = -c(conflict_id)) #Get rid of conflict_id
+
+conflict_data_bin <- conflict_data_bin %>% group_by(year, ISO) %>% 
+  summarize(
+    best = sum(best, na.rm = TRUE)
+  ) %>% mutate(conflict = ifelse(best >= 25, 1, 0))
+
+
+
