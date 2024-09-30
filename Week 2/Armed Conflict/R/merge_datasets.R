f9f7808d08882173230e7500b6c7d2895e853fed
Luke <lukeltb17@gmail.com>
2024-09-30 10:54:42 -0400

    Finished finaldata

--- Week 2/Armed Conflict/R/merge_datasets.R
+++ Week 2/Armed Conflict/R/merge_datasets.R
@@ -16,8 +16,8 @@ colnames(subset_grouped)
 
 final_merged_data <- merged_data %>%
   full_join(subset_grouped, by = c("ISO", "year")) %>%   # Merge with conflict data
-  full_join(covariates, select(ISO, year, country_names), by = c("ISO", "year")) %>% 
-  full_join(conflict_data_bin, by = c("ISO", "year"))  
+  full_join(conflict_data_bin, by = c("ISO", "year"))%>% 
+  left_join(covariates, select(ISO, year, country_names), by = c("ISO", "year"))
 
 
 #Reorder the column names to be similar to the example in week4_eda_inclass.pdf
@@ -31,7 +31,9 @@ corrected_order <- c("country_name", "ISO", "region", "year", "gdp1000",
 finaldata <- finaldata %>%
   mutate(earthquake = replace_na(earthquake, 0),
          drought = replace_na(drought, 0))
-
+#Remove every row with N/A country_name
+finaldata <- finaldata %>%
+  filter(!is.na(country_name))
 
 
 finaldata_reordered <- finaldata %>%
@@ -44,3 +46,4 @@ finaldata %>%
   dplyr::filter(country_name == "Canada")
 
 names(finaldata)
+
