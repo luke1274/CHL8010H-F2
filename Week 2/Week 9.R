8613dd9da6f7d85073c7d3251b1fcdff421f4e2f
Luke <lukeltb17@gmail.com>
2024-11-04 20:16:27 -0500

    Fixed Variable Names

--- Week 2/Week 9.R
+++ Week 2/Week 9.R
@@ -1,6 +1,8 @@
 library(here)
 library(plm)
 library(mice)
+library(texreg)
+library(here)
 
 data <- source("Armed Conflict/R/merge_datasets.R")
 
@@ -57,19 +59,19 @@ plot(mice.multi.out)
 
 ### MI analysis
 fit.mi.matmor <- with(mice.multi.out, 
-                      lm(matmor ~ -1 + armconf1 + loggdp + OECD + pctpopdens + urban + 
+                      lm(Maternal_Mortality_Rate ~ -1 + conflict + gdp1000 + OECD + popdens + urban + 
                            agedep + male_edu + temp + rainfall1000 + earthquake + drought + 
                            as.factor(ISOnum) + as.factor(year)))
 fit.mi.infmor <- with(mice.multi.out, 
-                      lm(infmor ~ -1 + armconf1 + loggdp + OECD + pctpopdens + urban + 
+                      lm(Infant_Mortality_Rate ~ -1 + conflict + gdp1000 + OECD + popdens + urban + 
                            agedep + male_edu + temp + rainfall1000 + earthquake + drought + 
                            as.factor(ISOnum) + as.factor(year)))
 fit.mi.neomor <- with(mice.multi.out, 
-                      lm(neomor ~ -1 + armconf1 + loggdp + OECD + pctpopdens + urban + 
+                      lm(NeoNatal_Mortality_Rate ~ -1 + conflict + gdp1000 + OECD + popdens + urban + 
                            agedep + male_edu + temp + rainfall1000 + earthquake + drought + 
                            as.factor(ISOnum) + as.factor(year)))
 fit.mi.un5mor <- with(mice.multi.out, 
-                      lm(un5mor ~ -1 + armconf1 + loggdp + OECD + pctpopdens + urban + 
+                      lm(Under5_Mortality_Rate ~ -1 + conflict + gdp1000 + OECD + popdens + urban + 
                            agedep + male_edu + temp + rainfall1000 + earthquake + drought + 
                            as.factor(ISOnum) + as.factor(year)))
 
@@ -80,22 +82,22 @@ out.un5mor <- pool(fit.mi.un5mor)
 
 ### CC analysis
 
-preds <- as.formula(" ~ -1 + armconf1 + loggdp + OECD + pctpopdens + urban + 
+preds <- as.formula(" ~ -1 + conflict + gdp1000 + OECD + popdens + urban + 
                   agedep + male_edu + temp + rainfall1000 + earthquake + drought + 
                   as.factor(ISO) + as.factor(year)")
 
-matmormod <- lm(update.formula(preds, matmor ~ .), data = finaldata)
-un5mormod <- lm(update.formula(preds, un5mor ~ .), data = finaldata)
-infmormod <- lm(update.formula(preds, infmor ~ .), data = finaldata)
-neomormod <- lm(update.formula(preds, neomor ~ .), data = finaldata)
+matmormod <- lm(update.formula(preds, Maternal_Mortality_Rate ~ .), data = finaldata)
+un5mormod <- lm(update.formula(preds, Under5_Mortality_Rate ~ .), data = finaldata)
+infmormod <- lm(update.formula(preds, Infant_Mortality_Rate ~ .), data = finaldata)
+neomormod <- lm(update.formula(preds, NeoNatal_Mortality_Rate ~ .), data = finaldata)
 
 tosave <- list(out.matmor, out.infmor, out.neomor, out.un5mor, 
                matmormod, un5mormod, infmormod, neomormod)
 
-keepvars <- list("armconf1" = "Armed conflict",
-                 "loggdp" = "log(GDP)",
+keepvars <- list("conflict" = "Armed conflict",
+                 "gdp1000" = "log(GDP)",
                  "OECD" = "OECD",
-                 "pctpopdens" = "Population density",
+                 "popdens" = "Population density",
                  "urban" = "Urban",
                  "agedep" = "Age dependency",
                  "male_edu" = "Male education",
@@ -108,4 +110,4 @@ screenreg(list(matmormod, out.matmor, un5mormod, out.un5mor, infmormod, out.infm
           custom.coef.map = keepvars,
           custom.model.names = c("Mat CC", "Mat MI", "Un5 CC", "Un5 MI", "Inf CC", "Inf MI", "Neo CC", "Neo MI"))
 
-save(tosave, file = here("output", "mi_output.Rdata"))
+#save(tosave, file = here("output", "mi_output.Rdata"))
